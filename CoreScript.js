38cd94abb4f1d85e399df30bb18dfc288ba6cd01
salmmanfred <32799244+salmmanfred@users.noreply.github.com>
2020-07-04 22:03:09 +0200

    Changes

---
+++ CoreScript.js
@@ -0,0 +1,24 @@
+//Have a good time coding
+function Changeweb(x) {
+  console.log("x");
+  if (x == 101) {
+    window.open("../Index.html", "_self");
+  }
+  if (x == 0) {
+    window.open("Index.html", "_self");
+  }
+  if (x == 1) {
+    window.open("pages/news.html", "_self");
+  }
+  if (x == 2) {
+    window.open("pages/Aboutus.html", "_self");
+  }
+}
+function Changeweb2(x) {
+  if (x == 2) {
+    window.open("Aboutus.html", "_self");
+  }
+  if (x == 1) {
+    window.open("news.html", "_self");
+  }
+}
